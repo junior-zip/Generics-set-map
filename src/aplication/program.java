bd570a180bf8979a23ead18d22b171d19fcf807e
Junior Teixeira <junior26bmx@gmail.com>
2022-09-14 12:30:24 -0300

    Usando generics para delimitar uma lista variada com tipos difrentes de itens compondo

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -1,2 +1,55 @@
-package aplication;public class program {
+package aplication;
+
+import entities.PrintService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) throws IllegalAccessException {
+        Scanner sc = new Scanner(System.in);
+
+        Locale.setDefault(Locale.US);
+
+        PrintService ps = new PrintService();
+
+
+        System.out.print("Digite uma quantidade: ");
+        int n = sc.nextInt();
+
+
+
+
+
+        for (int i= 0; i < n; i++){
+
+            Integer value = sc.nextInt();
+            ps.addValue(value);
+
+        }
+        sc.nextLine();
+        ps.print();
+        Object x = ps.first();
+
+
+        System.out.println("First: " + x);
+
+        Object y = ps.last();
+
+        System.out.println("Last: " + y);
+
+
+
+
+
+
+
+
+
+        sc.close();
+
+
+
+    }
+
 }
---
+++ src/entities/PrintService.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService<T> {
+
+    private List<T> list = new ArrayList<>();
+
+    public void addValue(T value){
+        list.add(value);
+
+    }
+
+    public T first() throws IllegalAccessException {
+        if (list.isEmpty()){
+            throw new IllegalAccessException("List is empty!");
+        }
+        return list.get(0);
+    }
+
+
+    public T last() throws IllegalAccessException{
+        if(list.isEmpty()){
+            throw new IllegalAccessException("List is empty!");
+
+        }
+
+        return list.get(list.size() - 1);
+
+    }
+
+    public void print(){
+        System.out.print("[");
+        if (!list.isEmpty()){
+            System.out.print(list.get(0));
+
+        }
+        for (int i=1; i<list.size(); i++){
+            System.out.print(", " + list.get(i));
+
+
+        }
+        System.out.println("]");
+    }
+
+
+
+
+
+
+}
